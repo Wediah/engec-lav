2e825ad2e4875ded47237bfaae843033a299b223
Wediah <100675006+Wediah@users.noreply.github.com>
2025-01-15 00:01:36 +0000

    updates footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -78,26 +78,80 @@
     </script>
 </div>
 
-<footer class="bg-transparent rounded-lg shadow m-4">
-    <div class="w-full mx-auto  p-6 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center">© 2024
-          <a href="/" class="hover:underline">ENGEC GROUP™</a>
-          . All Rights Reserved.
-    </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
-            <li>
-                <a href="#" class="hover:underline me-4 md:me-6">About</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline">Contact</a>
-            </li>
-        </ul>
+<footer class="bg-gradient-to-b from-blue-800 to-black p-6">
+    <div class="grid grid-cols-3 gap-12 items-center">
+        <div>
+            <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <span class="self-center text-6xl font-black whitespace-nowrap text-white">ENGEC GROUP</span>
+            </a>
+        </div>
+        <div>
+            <ul class="flex flex-wrap items-center gap-10 mt-3 font-bold text-white sm:mt-0 text-xl ">
+                <li>
+                    <a href="/about" class="hover:underline">About</a>
+                </li>
+                <li>
+                    <a href="/services" class="hover:underline">Services</a>
+                </li>
+                <li>
+                    <a href="{{ route('projects') }}" class="hover:underline">Our Works</a>
+                </li>
+                <li>
+                    <a href="/contact" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <div class="">
+            <ul class="flex flex-wrap items-center mt-3 gap-6 font-bold text-white sm:mt-0 text-xl justify-end">
+                <li>
+                    <a href="https://web.facebook.com/people/Engec-Group/100084279907473/?mibextid=ZbWKwL" class="font-bold text-4xl"><i class='bx bxl-facebook-square'></i></a>
+                </li>
+                <li>
+                    <a href="https://x.com/engecgroup" class="font-bold text-4xl"><i class='bx bxl-twitter' ></i></a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/engec-group-794298259/" class="font-bold text-4xl"><i class='bx bxl-instagram' ></i></a>
+                </li>
+                <li>
+                    <a href="mailto:groupengec@gmail.com" class="font-bold text-4xl"><i class='bx bxs-envelope'
+                        ></i></a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/engec-group-794298259/" class="font-bold text-4xl"><i class='bx bxl-linkedin-square'></i></a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+
+    <div class="grid grid-cols-3 gap-12 items-start mt-8 text-white">
+        <div>
+            <p>
+                © 2024
+                <a href="/" class="hover:underline">ENGEC GROUP™</a> <br>
+                 All Rights Reserved.
+            </p>
+        </div>
+        <div >
+            <p class="justify-end">Engec group is an Equal Opportunity Employer - race, color, religion, sex, sexual
+                orientation, gender
+                identity, national origin, disability, status as a protected veteran, or other characteristics protected by applicable law.</p>
+            <a href="#" class="text-sm font-bold">Human Rights Policy</a>
+        </div>
+        <div>
+            <ul class="flex flex-wrap items-center mt-3 gap-6 font-bold text-white sm:mt-0 text-sm justify-end">
+                <li class="relative after:content-['|'] after:mx-3 after:text-white last:after:content-none">
+                    <a href="#" class="hover:underline">License</a>
+                </li>
+                <li class="relative after:content-['|'] after:mx-3 after:text-white last:after:content-none">
+                    <a href="#" class="hover:underline">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Terms of Use</a>
+                </li>
+            </ul>
+
+        </div>
     </div>
 </footer>
 
